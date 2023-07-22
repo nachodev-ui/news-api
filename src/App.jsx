dd3ad5861a99eab155b550ee439a89d096a45c8c
nachodev-ui <ig.cisternas@duocuc.cl>
2023-07-22 15:41:10 -0400

    feat: NewsProvider added to App.jsx & some Material UI styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import { Container, Grid, Typography } from "@mui/material"
+import Formulario from "./components/Formulario"
+import { NewsProvider } from "./context/NewsProvider"
 
 function App() {
   return (
-    <Container>
-      <header>
-        <Typography align="center" marginY={5} component='h1' variant="h3">
-          Buscador de noticias
-        </Typography>
-      </header>
-    </Container>
+    <NewsProvider>
+      <Container>
+        <header>
+          <Typography align="center" marginY={5} component="h1" variant="h3">
+            Buscador de noticias
+          </Typography>
+        </header>
+
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item xs={10} md={6} lg={4}>
+            <Formulario />
+          </Grid>
+        </Grid>
+      </Container>
+    </NewsProvider>
   )
 }
 
