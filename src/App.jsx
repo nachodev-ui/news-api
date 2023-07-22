3aa22538211641bbb20c9315eb341783a9ed5ad5
nachodev-ui <ig.cisternas@duocuc.cl>
2023-07-22 15:18:55 -0400

    feat: News Search engine API

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Container, Grid, Typography } from "@mui/material"
+
+function App() {
+  return (
+    <Container>
+      <header>
+        <Typography align="center" marginY={5} component='h1' variant="h3">
+          Buscador de noticias
+        </Typography>
+      </header>
+    </Container>
+  )
+}
+
+export default App
