411d7d4c52d7560dbea906c3db0ec9c561d9168b
nachodev-ui <ig.cisternas@duocuc.cl>
2023-07-22 15:37:33 -0400

    feat: categories in form with mui/material components

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,54 @@
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  Box,
+} from "@mui/material"
+import useNews from "../hooks/useNews"
+
+const categories = [
+  { value: "general", label: "General" },
+  { value: "business", label: "Negocios" },
+  { value: "entertainment", label: "Entretenimiento" },
+  { value: "health", label: "Salud" },
+  { value: "science", label: "Ciencia" },
+  { value: "sports", label: "Deportes" },
+  { value: "technology", label: "Tecnología" },
+]
+
+const Formulario = () => {
+  const { category, handleChangeCategory } = useNews()
+
+  return (
+    <form>
+      <FormControl fullWidth>
+        <InputLabel>Categoría</InputLabel>
+        <Select
+          label="Categoría"
+          onChange={handleChangeCategory}
+          value={category}
+        >
+          {categories.map((category) => (
+            <MenuItem key={category.value} value={category.value}>
+              {category.label}
+            </MenuItem>
+          ))}
+        </Select>
+
+        <Box
+          sx={{
+            marginTop: 2,
+          }}
+        >
+          <Button fullWidth variant="contained" color="primary">
+            Buscar noticias
+          </Button>
+        </Box>
+      </FormControl>
+    </form>
+  )
+}
+
+export default Formulario
